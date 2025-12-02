85cfce938788fdcc4f03f28b5c666288692d80c3
Jeison Garcia <jt.garciaa@uea.edu.ec>
2025-12-02 14:32:47 -0500

    Tarea Semana 2

--- obj/Debug/net10.0/FigurasGeometricasPOO.AssemblyInfo.cs
+++ obj/Debug/net10.0/FigurasGeometricasPOO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FigurasGeometricasPOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4384b771bc976839afafc9de94446f9170ccb45c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FigurasGeometricasPOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FigurasGeometricasPOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
